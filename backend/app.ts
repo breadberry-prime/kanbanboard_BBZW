3400ea9119f69dcd2b1a6c5b18a479fc5af30244
= <=>
2021-11-29 21:12:50 +0100

    fixed app.ts wrong file mismasch, script is working now

--- backend/app.ts
+++ backend/app.ts
@@ -1,42 +1,16 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-import {createTask, getTasks} from "./services/Board.service.ts";
+import {Application} from "https://deno.land/x/oak/mod.ts";
+import {oakCors} from "https://deno.land/x/cors@v1.2.2/oakCors.ts"
+import {router} from "./router.ts"
 
-const router: Router = new Router()
+const port = 4040
+const app = new Application()
 
-router.get("/", (ctx) => {
-    ctx.response.body = "Hello World"
-})
+app.use(oakCors({
+    origin: '*'
+}));
 
-router
-    .get("/api", (ctx) => {
-        ctx.response.body = "movie db api documentation";
-    })
-    .get("/api/getTasks", (ctx) => {
-        ctx.response.body = getTasks()
-    })
-    .post("/api/createTask", async (ctx) => {
-        try {
-            const result = ctx.request.body();
-            if (result.type === "json") {
-                const taskBlueprint = await result.value;
+app.use(router.routes())
+app.use(router.allowedMethods())
 
-                const task = await createTask(taskBlueprint.value, taskBlueprint.column)
-
-                ctx.response.body = "task created id: " + task.id;
-                ctx.response.status = 200
-            } else {
-                console.log("err", result.type)
-                new Error("wrong body type")
-            }
-        }
-        catch (e){
-            console.log(e)
-        }
-    })
-
-
-console.log("loaded routes")
-
-export { router }
-
-// TODO clear overview
+console.log("http://localhost:" + port.toString())
+await app.listen({port})
